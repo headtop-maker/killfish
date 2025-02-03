2204b38c22c5681be3142fedaad5b34664c6584c
VadiuninDA <vadiuninda@lad24.ru>
2025-02-03 11:03:05 +0300

    debug: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import {Assets as NavigationAssets} from '@react-navigation/elements';
-import {Asset} from 'expo-asset';
-import {Provider} from 'react-redux';
-import * as SplashScreen from 'expo-splash-screen';
-import React from 'react';
-import {Navigation} from './navigation';
-import {store} from "./app/store";
+import { Assets as NavigationAssets } from "@react-navigation/elements";
+import { Asset } from "expo-asset";
+import { Provider } from "react-redux";
+import * as SplashScreen from "expo-splash-screen";
+import React from "react";
+import { Navigation } from "./navigation";
+import { store } from "./app/store";
 
 Asset.loadAsync([
-    ...NavigationAssets,
-    require('./assets/newspaper.png'),
-    require('./assets/bell.png'),
+  ...NavigationAssets,
+  require("./assets/newspaper.png"),
+  require("./assets/bell.png"),
 ]);
 
 SplashScreen.preventAutoHideAsync();
 
 export function App() {
-    return (
-        <Provider store={store}>
-            <Navigation
-                linking={{
-                    enabled: 'auto',
-                    prefixes: [
-                        'helloworld://',
-                    ],
-                }}
-                onReady={() => {
-                    SplashScreen.hideAsync();
-                }}
-            />
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <Navigation
+        linking={{
+          enabled: "auto",
+          prefixes: ["helloworld://"],
+        }}
+        onReady={() => {
+          SplashScreen.hideAsync();
+        }}
+      />
+    </Provider>
+  );
 }
